bd28b054390c088f33246c35d496dd012af3c92f
Fouzi92 <fouzi457@hotmail.fr>
2023-03-16 20:08:46 +0100

    Formation Php and Mysql

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/* echo "Hello World";
+
+// Les Variablen en Php 
+$age_du_lyceen =  18 ; // Entier (int)
+$age_du_lyceen =  18.5 ; // Float
+// $age_du_lyceen = "Ici c'est Paris" chaine de caractere ou string en anglais
+
+$est_majeur = false; // bool vrais ou faux
+
+// Convention Internationale ou Kamel anglais il faut mettre une majucule dans chacun 1er lettre d'un nouveau mot exsemple en bas
+
+$ageDuLyceen = 20;
+
+
+echo $age_du_lyceen; */
+
+// Concatenation de variable 
+$direBonjour =  "Aller";
+$distanation = "PARIS SG";
+
+echo $direBonjour . ' ' .$distanation. ' le football gagne !';
+
+?>
