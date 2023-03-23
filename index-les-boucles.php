53fd721ccff1d6aa2ea191decf3f29ed2224b77e
Fouzi92 <fouzi457@hotmail.fr>
2023-03-24 00:28:20 +0100

    Formation Php and Mysql

---
+++ index-les-boucles.php
@@ -0,0 +1,29 @@
+<?php
+// Les Boucles : While 
+
+    // $ligne = 0;
+
+    // $plus = 1;
+
+    // // Ma solution 
+    // while ($ligne < 10) {
+    // echo '<br/>Voici une ligne '. ' ' .$plus++;
+    // $ligne++;
+    // }
+
+ // la solution du formateur
+
+//   $ligneD = 0;
+
+//   while ($ligneD <10) {
+//     echo 'Voici le numéro de la ligne : '.($ligneD+1).' <br/>';
+//     $ligneD++;
+//   } 
+
+// Les Boucles : For 
+ 
+for ($i = 0; $i <10; $i++) {
+    echo 'Voici le numéro de la ligne :'.($i+1).'<br/>';
+}
+ 
+ ?>
