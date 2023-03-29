3536ac85579d9a1ba4a23aed6c5ec2b49510ef0d
Fouzi92 <fouzi457@hotmail.fr>
2023-03-29 23:55:35 +0200

    Formation Php and Mysql

---
+++ index-3.php
@@ -0,0 +1,35 @@
+// Les Fonction en PHP NATIVES 
+
+/* $string = "Bienvenue sur la formation ultime en Php et MySQL";
+
+ // sTRLEN
+
+ echo 'Nombre de caractéres :'.strlen($string).'<br/>';
+
+ // STR_Replace
+ 
+ echo 'La string transformée :'.str_replace('Bienvenue','Welcome',$string).'<br/>';
+
+ //Strtolower 
+
+ echo strtolower($string).'<br/>';
+ echo strtoupper($string).'<br/>';
+
+ // SUBSTR
+
+ echo substr($string, 0, 9).'<br/>'; */
+ 
+ ///////// Les Fonction en PHP NATIVES Math///////////
+
+ // abs pour trouver le nombre absolu " echo abs(82);"
+
+ // Max pour trouver le grand nombre
+ 
+// Min pour trouver le plus petit nombre
+
+// Rand pour genere un nombre aleatoire
+
+ ///////// Les Fonction en PHP NATIVES les Tableau ///////////
+
+ $array = array("Pessi","Peymar","Penzard");
+ 
